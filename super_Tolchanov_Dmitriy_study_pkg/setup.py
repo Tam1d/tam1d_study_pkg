72922883684e7209318bc8b6c029627045213163
Tam1d <tamid777@mail.ru>
2026-03-29 16:18:11 +0300

    lab3_task1

--- super_Tolchanov_Dmitriy_study_pkg/setup.py
+++ super_Tolchanov_Dmitriy_study_pkg/setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup
 
 package_name = 'super_Tolchanov_Dmitriy_study_pkg'
@@ -10,6 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), ['launch/my_first_launch.py']),
+        (os.path.join('share', package_name, 'launch'), ['launch/robot_system.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
